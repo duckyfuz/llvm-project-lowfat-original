8c1ecbb636c2cc77b1ab714ffa6b5547af1bbfde
duckyfuz <108561447+duckyfuz@users.noreply.github.com>
2026-03-01 15:47:10 +0800

    remove redef nothrow_t

--- llvm-4.0.0.src/projects/compiler-rt/lib/scudo/scudo_new_delete.cpp
+++ llvm-4.0.0.src/projects/compiler-rt/lib/scudo/scudo_new_delete.cpp
@@ -21,11 +21,6 @@ using namespace __scudo;
 
 #define CXX_OPERATOR_ATTRIBUTE INTERCEPTOR_ATTRIBUTE
 
-// Fake std::nothrow_t to avoid including <new>.
-namespace std {
-struct nothrow_t {};
-}  // namespace std
-
 CXX_OPERATOR_ATTRIBUTE
 void *operator new(size_t size) {
   return scudoMalloc(size, FromNew);
